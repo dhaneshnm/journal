3d3638a594256887c2dfc45cf0f5a5eb50c63e12
Dhanesh Neela Mana <dhanesh.n.m19@gmail.com>
2025-03-26 13:40:24 -0500

    delete tags

--- src/routes/tag/+page.server.ts
+++ src/routes/tag/+page.server.ts
@@ -1,5 +1,34 @@
 import prisma from "$lib/prisma";
 import type { PageServerLoad } from "../$types";
+import { page } from '$app/stores'
+import { fail, redirect } from '@sveltejs/kit';
+
+
+async function deleteEntryWithTags(tagId: number) {
+    // First, delete the tags associated with the journal entry
+    await prisma.tagOnEntries.deleteMany({
+        where: { tagId: tagId }
+    });
+
+    // Then, delete the journal entry itself
+    await prisma.tag.delete({
+        where: { id: tagId }
+    });
+}
+
+export const actions = {
+    default: async ({ url }) => {
+        let id = url.searchParams.get('id');
+        console.log("here", url, id); 
+        try {
+            await deleteEntryWithTags(Number(id));
+            throw redirect(303, '/tag');
+        } catch (error) {
+            console.log(error);
+            return fail(500, { error: 'Failed to delete entry' });
+        }
+    }
+};
 
 export const load = (async () => {
     const response = await prisma.tag.findMany({
--- src/routes/tag/+page.svelte
+++ src/routes/tag/+page.svelte
@@ -1,6 +1,28 @@
 <script lang="ts">
-    import type { PageData } from './$types'
+    import type { PageData } from './$types';
+    import { invalidateAll } from '$app/navigation';
     export let data: PageData;
+    function handleDelete(id: number) {
+        if (confirm('Are you sure you want to delete this tag?')) {
+        fetch(`/tag/?id=${id}`, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({
+            _method: 'delete'
+            })
+        }).then(response => {
+            if (response.ok) {
+                invalidateAll();
+            } else {
+            console.error('Failed to delete entry');
+            }
+        }).catch(error => {
+            console.error('Error:', error);
+        });
+        }
+  }
 </script>
 <section>
     <h2>Tags</h2>
@@ -8,7 +30,8 @@
         {#if data.tags.length > 0 }
             {#each data.tags as tag}
                 <div class="card entry-card">
-                    <h3>{tag.name}</h3>
+                    <h3>{tag.name}| <button class="danger" on:click={ () => handleDelete(tag.id) }>Delete</button></h3>
+                    
                     {#each tag.journalEntries as entry}
                         <span>
                             <a href="/entry/{entry.journalEntry.id}" target="_blank">{entry.journalEntry.title}</a>
